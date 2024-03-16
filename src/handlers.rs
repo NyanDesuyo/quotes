ac9df349da1fcf0acb1d5913f291227033336ed0
NyanDesuyo <gerogeroe12@gmail.com>
2024-03-16 22:46:47 +0700

    read quotes

--- src/handlers.rs
+++ src/handlers.rs
@@ -59,3 +59,20 @@ pub async fn create_quote(
         Err(_) => Err(http::StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
+
+pub async fn read_quotes(
+    extract::State(pool): extract::State<PgPool>,
+) -> Result<axum::Json<Vec<Quote>>, http::StatusCode> {
+    let res = sqlx::query_as(
+        r#"
+        SELECT * FROM quotes
+        "#,
+    )
+    .fetch_all(&pool)
+    .await;
+
+    match res {
+        Ok(quotes) => Ok(axum::Json(quotes)),
+        Err(_) => Err(http::StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
